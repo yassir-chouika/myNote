3661aa646a7f0c9b1dbc73d810cfd9cc4a0d4b69
CHOUIKA Yassir <yassir.chouika@gmail.com>
2025-05-27 14:48:01 +0200

    Initial commit

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import "../global.css";
+
+const Home = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-[#114B5F]">
+      <Text className="color-orange-50 text-xl font-bold">You're Good To Go !</Text>
+    </View>
+  );
+};
+
+export default Home;
+
